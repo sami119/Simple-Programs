9476bb673e9a72dd64377d4762711f2a1cfe70b4
Samuil <35292702+sami119@users.noreply.github.com>
2018-04-21 22:12:12 +0300

    new program

---
+++ Програми/Visual Studio Projects/From Lessons/Person2/Person/Person.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Person
+    {
+        // declare some variables
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double salary;
+
+        // our constructor
+        public Person(string firstName, string lastName, int age, double salary)
+        {
+            if (firstName.Count() < 3)
+            {
+                throw new ArgumentException("First name cannot be less than 3 symbols");
+            }
+            else
+            {
+                this.firstName = firstName;
+            }
+
+            if (lastName.Count() < 3)
+            {
+                throw new ArgumentException("Last name cannot be less than 3 symbols");
+            }
+            else
+            {
+                this.lastName = lastName;
+            }
+
+            if (age <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or negative integer");
+            }
+            else
+            {
+                this.age = age;
+            }
+
+            if (salary < 460)
+            {
+                throw new ArgumentException("Salary cannot be less than 460 leva");
+            }
+            else
+            {
+                this.salary = salary;
+            }
+        }
+
+        //public variables
+        /*public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (value.Count() < 3)
+                {
+                    throw new ArgumentException("First name cannot be less than 3 symbols");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (value.Count() < 3)
+                {
+                    throw new ArgumentException("Last name cannot be less than 3 symbols");
+                }
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or negative integer");
+                }
+                this.age = value;
+            }
+        }
+
+        public double Salary
+        {
+            get { return this.salary; }
+            set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva");
+                }
+                this.salary = value;
+            }
+        }*/
+
+        // methods
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} get {this.salary:f2} leva";
+        }
+
+        public void IncreaseSalary(double bonus)
+        {
+            double multiplyer = 1.0 + (bonus / 100);
+            this.salary *= multiplyer;
+        }
+
+    }
+}
---
+++ Програми/Visual Studio Projects/From Lessons/Person2/Person/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var persons = new List<Person>();
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), double.Parse(cmdArgs[3]));
+                persons.Add(person);
+            }
+
+            var bonus = double.Parse(Console.ReadLine());
+
+            //persons.ForEach(p => p.IncreaseSalary(bonus));
+            foreach(var p in persons)
+            {
+                p.IncreaseSalary(bonus);
+            }
+
+            //persons.ForEach(p => Console.WriteLine(p.ToString()));
+            foreach(var p in persons)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
