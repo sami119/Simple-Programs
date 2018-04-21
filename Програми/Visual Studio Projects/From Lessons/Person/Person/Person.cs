51ac9a31fcd2e87cbba7975aac73113fe1d3ba63
Samuil <35292702+sami119@users.noreply.github.com>
2018-04-21 15:09:14 +0300

    New tasks

---
+++ Програми/Visual Studio Projects/From Lessons/Person/Person/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        public string FirstName
+        {
+            get { return this.firstName; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+        }
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} is a {this.age} years old";
+        }
+    }
+}
