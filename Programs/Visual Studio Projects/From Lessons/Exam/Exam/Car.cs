1bb9111ae3016bebc0826c639c704266d5dc2ddf
Samuil <35292702+sami119@users.noreply.github.com>
2018-04-29 17:33:20 +0300

    new program

---
+++ Programs/Visual Studio Projects/From Lessons/Exam/Exam/Car.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Car
+    {
+        private string model;
+        private string manufacturer;
+        private double loadCapacity;
+        private List<Part> parts;
+        private double fuel = 100;
+        private static int ordersCount = 0;
+
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            set
+            {
+                if (value.Length < 5)
+                { throw new FormatException("Invalid manufacturer name!"); }
+                else { this.manufacturer = value; }
+            }
+        }
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (value.Length < 3)
+                { throw new FormatException("Invalid model name!"); }
+                else { this.model = value; }
+            }
+        }
+        public double LoadCapacity
+        {
+            get { return loadCapacity; }
+            set
+            {
+                if (value < 0)
+                { throw new FormatException("Invalid load capacity!"); }
+                else
+                { this.loadCapacity = value; }
+            }
+        }
+
+        public Car(string manufacturer, string model, double loadCapacity)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.LoadCapacity = loadCapacity;
+            this.parts = new List<Part>();
+            ordersCount++;
+        }
+        public double GetCarPrice()
+        {
+            double sum = 0;
+            foreach (Part p in parts)
+            {
+                sum += p.Price;
+            }
+            return sum;
+        }
+
+        public ReadOnlyCollection<Part> Parts
+        {
+            get { return new ReadOnlyCollection<Part>(parts); }
+        }
+
+        public void AddPart(Part part)
+        {
+            parts.Add(part);
+        }
+        public void AddMultipleParts(List<Part> passedParts)
+        {
+            foreach (Part p in passedParts)
+            {
+                parts.Add(p);
+            }
+        }
+        public void RemovePartByName(string name)
+        {
+            parts = parts.FindAll(p => p.Name != name);
+        }
+        public List<Part> GetPartsWithPriceAbove(double price)
+        {
+            return parts.FindAll(p => p.Price > price).ToList();
+        }
+        public Part GetMostExpensivePart()
+        {
+            double minPrice = 0;
+            Part result = null;
+            foreach (Part p in parts)
+            {
+                if (p.Price > minPrice)
+                {
+                    minPrice = p.Price;
+                    result = p;
+                }
+            }
+            return result;
+        }
+
+        public static int OrdersCount
+        {
+            set { ordersCount = value; }
+            get { return ordersCount; }
+        }
+
+
+        public void Drive(double distance)
+        {
+            if ((loadCapacity * 0.2 * distance) > fuel)
+            {
+                throw new ArgumentException("Drive not possible!");
+            }
+            else
+            {
+                fuel = loadCapacity * 0.2 * distance;
+            }
+        }
+        public bool ContainsPart(string partName)
+        {
+            foreach (Part p in parts)
+            {
+                if (p.Name == partName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string s;
+            double pricet = 0;
+            s = Model.ToUpper().ToString() + " made by " + Manufacturer.ToString() + "\n";
+            s = s + "Available parts: \n";
+            foreach (Part i in parts)
+            {
+                s = s + string.Format("-> {0} - {1:F2} \n", i.Name, i.Price);
+                pricet += i.Price;
+            }
+            s = s + string.Format("With total price of: {0:F2} lv.", pricet);
+            return s;
+        }
+    }
+}
---
+++ Programs/Visual Studio Projects/From Lessons/Exam/Exam/Part.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Part
+    {
+        private string name;
+        private double price;
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (value.Length < 5)
+                {
+                    throw new FormatException("Invalid part name!");
+                }
+                else { this.name = value; }
+            }
+        }
+
+        public double Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new FormatException("Price should be positive!");
+                }
+                else
+                {
+                    this.price = value;
+                }
+            }
+        }
+
+        public Part(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public Part(string name)
+        {
+            this.name = name;
+            this.price = 25;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("-> {0} - {1:F2}", this.name, this.price);
+        }
+    }
+}
