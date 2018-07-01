ac3c1e3a9536f628babe0509a331d511a439b8d8
Samuil <35292702+sami119@users.noreply.github.com>
2018-07-01 09:12:15 +0300

    new programs

---
+++ Programs/Visual Studio Projects/From Lessons/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int br = 0, num = 0, maxNum = 0, maxBr = 0;
+            num = arr[0];
+            br = 1;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] == arr[i-1])
+                {
+                    br++;
+                }
+                else if(br > maxBr)
+                {
+                    maxBr = br;
+                    maxNum = num;
+                    num = arr[i];
+                    br = 1;         
+                }
+            }
+
+            if (br > maxBr)
+            {
+                maxBr = br;
+                maxNum = num;
+            }
+
+            for (int i = 0; i < maxBr; i++)
+            {
+                Console.Write("{0} ", maxNum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programs/Visual Studio Projects/From Lessons/masiv/masiv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            if (arr.Contains(n))
+            {
+                Console.WriteLine("{0} Exists in the List", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} Not exists in the List", n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
