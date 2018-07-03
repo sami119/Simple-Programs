395559ffed1f5acd601e5c43709c81350bd71917
Samuil <35292702+sami119@users.noreply.github.com>
2018-07-03 14:34:28 +0300

    new programs

---
+++ Programs/Visual Studio Projects/From Lessons/Modul 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            string command = Console.ReadLine();
+            string previous = command;
+
+            while (command.ToLower() != "exit")
+            {
+                if (command == "back")
+                {
+                    if (stack.Count != 0)
+                    {
+                        Console.WriteLine(stack.Pop());
+                    }
+                    previous = null;
+                }
+                else
+                {
+                    if (previous != null)
+                    {
+                        stack.Push(previous);
+                    }
+                    previous = command;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programs/Visual Studio Projects/From Lessons/Modul 4/Problem1/Problem1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        }
+    }
+}
