b2673979b0853142c4399210a1fd9aecaea4728c
Samuil <35292702+sami119@users.noreply.github.com>
2018-07-05 15:50:06 +0300

    this is test

--- Programs/Visual Studio Projects/From Lessons/Modul 4/SearchingMethods/SearchingMethods/Program.cs
+++ Programs/Visual Studio Projects/From Lessons/Modul 4/SearchingMethods/SearchingMethods/Program.cs
@@ -10,6 +10,11 @@ namespace SearchingMethods
     {
         static void Main(string[] args)
         {
+            int[] N_C = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] C = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] N = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
         }
     }
 }
--- Programs/Visual Studio Projects/From Lessons/Modul 4/SearchingMethods/SearchingMethods/SearchingMethods.cs
+++ Programs/Visual Studio Projects/From Lessons/Modul 4/SearchingMethods/SearchingMethods/SearchingMethods.cs
@@ -8,5 +8,68 @@ namespace SearchingMethods
 {
     class SearchingMethods
     {
+        // 1. LinearSearch = O(N)
+        public static int LinearSearch<T>(T[] array, T key) where T : IComparable
+        {
+            for (int index = 0; index < array.Count(); index++)
+            {
+                if (array[index].CompareTo(key) == 0)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        // 2. RecursiveBinarySearch = O(log(N))
+        public static int RecursiveBinarySearch<T>(T[] array, T key, int start, int end) where T : IComparable
+        {
+            if (end < start) return -1;
+            int mid = (start + end) / 2;
+            if (array[mid].CompareTo(key) > 0) return RecursiveBinarySearch(array, key, start, mid - 1);
+            else if (array[mid].CompareTo(key) < 0) return RecursiveBinarySearch(array, key, mid + 1, end);
+            else return mid;
+        }
+
+        // 3. BinarySearch = O(log(N))
+        public static int BinarySearch<T>(T[] array, T key, int start, int end) where T : IComparable
+        {
+            while (end >= start)
+            {
+                int mid = (start + end) / 2;
+                if (array[mid].CompareTo(key) > 0) end = mid - 1;
+                else if (array[mid].CompareTo(key) < 0) start = mid + 1;
+                else return mid;
+            }
+            return -1;
+        }
+
+        // 4. InterpolationSearch = O(log(log(N)))
+        public static int InterpolationSearch<T>(T[] array, T key) where T : IComparable
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            while (array[low].CompareTo(key) <= 0 && array[high].CompareTo(key) >= 0)
+            {
+                int mid = low + (((dynamic)key - (dynamic)array[low]) * (high - low)) / ((dynamic)array[high] - (dynamic)array[low]);
+                if (array[mid].CompareTo(key) < 0) low = mid + 1;
+                else if (array[mid].CompareTo(key) > 0) high = mid - 1;
+                else return mid;
+            }
+            return -1;
+        }
+
+        // 5. GoodSearch = O(N)
+        public static int EasySearch<T>(T[] array, T key) where T : IComparable
+        {
+            for (int index = array.Length - 1; index != 0; index--)
+            {
+                if (array[index].Equals(key))
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
     }
 }
