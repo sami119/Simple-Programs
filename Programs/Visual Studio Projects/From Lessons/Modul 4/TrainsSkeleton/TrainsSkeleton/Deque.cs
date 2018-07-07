b306c817267df2b1a9d31a4996c27476088e648a
Samuil <35292702+sami119@users.noreply.github.com>
2018-07-07 11:22:11 +0300

    new program

---
+++ Programs/Visual Studio Projects/From Lessons/Modul 4/TrainsSkeleton/TrainsSkeleton/Deque.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainsSkeleton
+{
+    public class Deque<T>
+    {
+        private const int defaultCapacity = 16;
+        public int Capacity { get; private set; } //показва капацитета
+        public int Count { get; private set; } //показва броят елементи
+        public T[] collection;
+
+        public Deque() : this(defaultCapacity)
+        {
+            //празен конструктор, задава капацитета на дека на стойността по подразбиране (16)
+        }
+
+        public Deque(int capacity)
+        {
+            //създава дека с точно зададен капацитет
+            collection = new T[capacity];
+            Capacity = capacity;
+            Count = 0;
+        }
+
+        public Deque(IEnumerable<T> collection) : this(collection.Count())
+        {
+            collection = collection.ToArray();
+            Capacity = collection.Count();
+            Count = collection.Count();
+            //създава дека с капацитет съответстващ на посочената колекция и прехвърля елементите от колекцията в дека
+        }
+
+        public void AddFront(T item)
+        {
+            if(Capacity == Count)
+            {
+                Capacity *= 2;
+                Array.Resize(ref collection, Capacity);
+            }
+            collection[Count++] = item;
+            //добавя елемент отпред
+        }
+
+        public void AddBack(T item)
+        {
+            if(Capacity == Count)
+            {
+                Capacity *= 2;
+                Array.Resize(ref collection, Capacity);
+            }
+            collection = new T[] { item }.Concat(collection.Take(Capacity - 1)).ToArray();
+            Count++;
+            //добавя елемент отзад
+        }
+
+        public T RemoveFront()
+        {
+            var item = collection[--Count];
+            collection = collection.Take(Count).Concat(collection.Skip(Count + 1).Concat(new T[1])).ToArray();
+            return item;
+            //връща и премахва елемента отпред
+        }
+
+        public T RemoveBack()
+        {
+            var item = collection[0];
+            Count--;
+            collection = collection.Skip(1).Concat(new T[1]).ToArray();
+            return item;
+            //връща и премахва елемента отзад
+        }
+
+        public T GetFront()
+        {
+            return collection[Count-1];
+            //връща, без да премахва, елемента отпред
+
+        }
+
+        public T GetBack()
+        {
+            return collection[0];
+            //връща, без да премахва, елемента отзад
+        }
+    }
+}
---
+++ Programs/Visual Studio Projects/From Lessons/Modul 4/TrainsSkeleton/TrainsSkeleton/Train.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainsSkeleton
+{
+    class Train
+    {
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                this.number = value;
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        private string type;
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                this.type = value;
+            }
+        }
+
+        private int cars;
+        public int Cars
+        {
+            get
+            {
+                return cars;
+            }
+            set
+            {
+                this.cars = value;
+            }
+        }
+
+        //Constructor
+        public Train(int number, string name, string type, int cars)
+        {
+            this.number = number;
+            this.name = name;
+            this.type = type;
+            this.cars = cars;
+        }
+
+        public override string ToString()
+        {
+            return Number + " " + Name + " " + Type + " " + Cars;
+        }
+    }
+}
