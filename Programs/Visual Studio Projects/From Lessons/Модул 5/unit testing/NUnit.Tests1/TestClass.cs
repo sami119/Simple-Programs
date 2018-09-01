497b15323bfb311434e183ec22c351ace967ab6d
Samuil <35292702+sami119@users.noreply.github.com>
2018-09-01 21:58:40 +0300

    new programs

---
+++ Programs/Visual Studio Projects/From Lessons/Модул 5/unit testing/NUnit.Tests1/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class BankAcountTests
+    {
+        [Test]
+        public void AcountInitializeWithPositiveValue()
+        {
+            BankAcount acount = new BankAcount(2000m);
+            Assert.AreEqual(2000m, acount.Amount);
+        }
+    }
+}
---
+++ Programs/Visual Studio Projects/From Lessons/Модул 5/Скелет/Lab/NUnit.Tests1/AxeTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLosesDurabilyAfterAttack()
+        {
+            // TODO: Add your test code here
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+
+            Assert.AreEqual(1, axe.DurabilityPoints, "Axe Durability doesnt change after attack");
+        }
+
+        [Test]
+        public void BrokenAxeCantAttack()
+        {
+            Axe axe = new Axe(2, 2);
+            Dummy dummy = new Dummy(20, 20);
+
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+        }
+    }
+}
