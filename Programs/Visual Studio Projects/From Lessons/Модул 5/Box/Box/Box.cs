b3ec95baced80b11fd3d9fa4d45858cf3d902060
Samuil <35292702+sami119@users.noreply.github.com>
2018-09-03 12:21:41 +0300

    box program

---
+++ Programs/Visual Studio Projects/From Lessons/Модул 5/Box/Box/Box.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public class Box<T>: IEnumerable<T>
+    {
+        private List<T> store;
+
+        public Box()
+        {
+            this.store = new List<T>();
+            this.count = 0;
+        }
+
+        private int count;
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public void Add(T input)
+        {
+            this.store.Add(input);
+            count++;
+        }
+
+        public T Remove()
+        {
+            T temp = store.Last();
+            this.store.Remove(store.Last());
+            count--;
+            return temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)store).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)store).GetEnumerator();
+        }
+    }
+}
---
+++ Programs/Visual Studio Projects/From Lessons/Модул 5/Box/Box/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(string.Join(" ", box));
+            foreach (var item in box)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
